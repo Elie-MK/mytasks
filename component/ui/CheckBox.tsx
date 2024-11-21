682a8c063ec7ddb693c63541d7d8b0457d74a15d
Elie-MK <elie.mk25@gmail.com>
2024-11-21 22:39:31 +0100

    add custom checkbox

---
+++ component/ui/CheckBox.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { CheckBox } from "@rneui/themed";
+
+type Props = {
+  check: boolean;
+  hanndleCheck: () => void;
+  title: string;
+};
+
+const CustomCheckBox = (props: Props) => {
+  return (
+    <CheckBox
+      title={props.title}
+      checkedIcon="dot-circle-o"
+      uncheckedIcon="circle-o"
+      checked={props.check}
+      onPress={props.hanndleCheck}
+      containerStyle={styles.checkBoxContainer}
+      fontFamily="Roboto-regular"
+      textStyle={{ fontSize: 18 }}
+    />
+  );
+};
+
+export default CustomCheckBox;
+
+const styles = StyleSheet.create({
+  checkBoxContainer: {
+    marginBottom: -15,
+    marginLeft: -10,
+  },
+});
