7fc6265fa2acded540027d0e212f66f14e36db70
Elie-MK <elie.mk25@gmail.com>
2024-12-01 20:57:24 +0100

    add dummyUsers

---
+++ screens/HomeScreens/Create_Task/ListOfCoworkers/dummyUsers.ts
@@ -0,0 +1,94 @@
+import { IUser } from "../../../../interfaces/IUser";
+
+export const users: IUser[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 2,
+    name: "Willliam Doe",
+    jobTitle: "Developer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 3,
+    name: "Mark Doe",
+    jobTitle: "Developer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 4,
+    name: "Maximilian Doe",
+    jobTitle: "Backend Developer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 5,
+    name: "Messi Doe",
+    jobTitle: "Software Engineer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 6,
+    name: "Jean Doe",
+    jobTitle: "Frontend Developer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 7,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 8,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 9,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 10,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 11,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 12,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 13,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 14,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+  {
+    id: 15,
+    name: "Kian Doe",
+    jobTitle: "UI/UX Designer",
+    image: "https://randomuser.me/api/portraits/men/36.jpg",
+  },
+];
