406702140c544148e422bb3e4f038714fe267b09
Elie-MK <elie.mk25@gmail.com>
2024-12-01 20:57:11 +0100

    add rootParamaList

---
+++ types/RootStackParamList.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+  CreateTask: { datas: number[] };
+  Coworker: { datas: number[] };
+  Onboarding: undefined;
+  SignIn: undefined;
+  SignUp: undefined;
+  HomeMain: undefined;
+  TaskDetail: undefined;
+};
