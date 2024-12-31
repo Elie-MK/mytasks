8cbd944c1f136e9189414a38c4493fc8bf72ad89
Elie-MK <elie.mk25@gmail.com>
2024-12-31 17:17:14 +0100

    refactor to save the state when the user click the 'Get Started' button

--- screens/Onboarding/Onboarding.tsx
+++ screens/Onboarding/Onboarding.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ParamListBase } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { SafeAreaView, StyleSheet, View } from "react-native";
@@ -13,14 +14,16 @@ type Props = {
 };
 
 const Onboarding = (props: Props) => {
+  function handleGetStarted() {
+    AsyncStorage.setItem("onboarded", "true");
+    props.navigation.replace("SignIn");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <OnboardingList />
       <View style={styles.buttonContainer}>
-        <Button
-          title="Get Started"
-          onPress={() => props.navigation.replace("SignIn")}
-        />
+        <Button title="Get Started" onPress={handleGetStarted} />
       </View>
     </SafeAreaView>
   );
