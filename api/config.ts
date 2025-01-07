ea7abcd4246b4809ae68f41ed76355eef8734997
Elie-MK <elie.mk25@gmail.com>
2025-01-07 20:27:47 +0100

    allow auto refresh token when is expired

--- api/config.ts
+++ api/config.ts
@@ -24,4 +24,36 @@ api.interceptors.request.use(async (config) => {
   }
 });
 
+// Refresh token flow
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      const refreshToken = await AsyncStorage.getItem("refresh_token");
+      if (refreshToken) {
+        try {
+          const response = await axios.post(
+            "http://localhost:8080/api/v1/auth/token/refresh",
+            {
+              refreshToken: refreshToken,
+            }
+          );
+          const newToken = response.data.data.jwt_token;
+          await AsyncStorage.setItem("token", newToken);
+          await AsyncStorage.setItem(
+            "refresh_token",
+            response.data.data.refresh_token
+          );
+          error.config.headers.Authorization = `Bearer ${newToken}`;
+          return axios(error.config);
+        } catch (refreshError) {
+          console.log("Refresh token expired or invalid.");
+          return Promise.reject(refreshError);
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
