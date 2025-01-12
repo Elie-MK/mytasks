358c344235ca91e0dc60cb92040a43ce4e6fc767
Elie-MK <elie.mk25@gmail.com>
2025-01-12 22:50:00 +0100

    add new path url to retrieve the current user

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -6,6 +6,7 @@ export const API_ENDPOINTS = {
     LOGIN: "/auth/login",
     REGISTER: "/auth/register",
     REGISTERASADMIN: "/auth/admin/register",
+    CURRENT_USER: "/auth/current-user",
   },
   TASK: {
     MY_TASKS: (sort: Sort) =>
--- api/services/auth.service.ts
+++ api/services/auth.service.ts
@@ -3,7 +3,7 @@ import { ISignup } from "../../interfaces/ISignup";
 import api from "../config";
 import { API_ENDPOINTS } from "../endpoints";
 import { ApiResponse } from "../types/api.types";
-import { JwtToken } from "../types/models";
+import { JwtToken, UserResponse } from "../types/models";
 
 export const authServices = {
   login: async (data: ISignin) => {
@@ -17,6 +17,12 @@ export const authServices = {
     const response = await api.post<ApiResponse<JwtToken>>(
       API_ENDPOINTS.AUTH.REGISTER,
       data
+    );
+    return response.data;
+  },
+  currentUser: async () => {
+    const response = await api.get<ApiResponse<UserResponse>>(
+      API_ENDPOINTS.AUTH.CURRENT_USER
     );
     return response.data;
   },
